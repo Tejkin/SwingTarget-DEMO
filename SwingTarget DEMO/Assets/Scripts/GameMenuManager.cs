22676396acc912a4dd859d587a83443086d97064
Tejkin <Thomas1467@hotmail.com>
2023-11-07 02:06:59 +0800

    added resume, restart and quit functionality

--- SwingTarget DEMO/Assets/Scripts/GameMenuManager.cs
+++ SwingTarget DEMO/Assets/Scripts/GameMenuManager.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class GameMenuManager : MonoBehaviour
 {
     public GameObject menu;
     public InputActionProperty showButton;
+    public bool paused = false;
     void Start()
     {
         
@@ -17,7 +19,39 @@ public class GameMenuManager : MonoBehaviour
     {
         if (showButton.action.WasPerformedThisFrame())
         {
-            menu.SetActive(!menu.activeSelf);
+            if (!paused)
+            {
+                Pause();
+            }
+            else if (paused)
+            {
+                Resume();
+            }
         }
     }
+
+    public void Pause()
+    {
+        Time.timeScale = 0;
+        menu.SetActive(!menu.activeSelf);
+        paused = true;
+    }
+
+    public void Resume()
+    {
+        Time.timeScale = 1;
+        menu.SetActive(!menu.activeSelf);
+        paused = false;
+    }
+    
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
 }
+
